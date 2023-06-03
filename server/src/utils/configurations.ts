4f7a25022735197603b2670e2e60a6df168015cf
ayobami <ayobami_adejumo@yahoo.com>
2023-06-03 05:38:26 -0700

    adding realance property

--- server/src/utils/configurations.ts
+++ server/src/utils/configurations.ts
@@ -1,8 +1,10 @@
 import SES from "aws-sdk/clients/ses"
 import config from "config"
+import dotenv from'dotenv';
+dotenv.config()
 
 const awsConfig = {
-    accessKeyId: config.get<string>('AWS_ACCESS_KEY_ID'),
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: config.get<string>('AWS_SECRET_ACCESS_KEY'),
     region: config.get<string>('region'),
     apiVersion: config.get<string>('apiVersion')
