58a2b3c0872ee3afec423c7480d24936c2068139
ayobami <ayobami_adejumo@yahoo.com>
2023-06-03 06:52:46 -0700

    adding default config

--- server/src/utils/configurations.ts
+++ server/src/utils/configurations.ts
@@ -1,13 +1,11 @@
-import SES from "aws-sdk/clients/ses"
-import config from "config"
-import dotenv from'dotenv';
-dotenv.config()
+import SES from "aws-sdk/clients/ses";
+import config from "config";
 
 const awsConfig = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: config.get<string>('AWS_SECRET_ACCESS_KEY'),
-    region: config.get<string>('region'),
-    apiVersion: config.get<string>('apiVersion')
-}
+  accessKeyId: config.get<string>("AWS_ACCESS_KEY_ID"),
+  secretAccessKey: config.get<string>("AWS_SECRET_ACCESS_KEY"),
+  region: config.get<string>("region"),
+  apiVersion: config.get<string>("apiVersion"),
+};
 
-export const AWSSES = new SES(awsConfig)
+export const AWSSES = new SES(awsConfig);
